d1e83a7e0b4b364149258a749c75f2d5e29fa884
Antoine Aumjaud <antoine_dev@aumjaud.fr>
2017-06-11 10:33:26 +0200

    :sparkles: add authent management

--- src/main/server.js
+++ src/main/server.js
@@ -1,28 +1,43 @@
 const APP_NAME = "api-family";
 const APP_VERSION = "0.0.1";
 
+let config = require('./conf/api-family.json');
 
 const express = require('express');
 const app = express()
+
+/*
+ * SECURITY
+ */
+const requireAuthentication = function (req, res, next) {
+    if(req.header("secure-key") && req.header("secure-key") === config.secureKey) {
+        next();
+    }
+    else {
+        res.status(401).send('Not authorized');
+    }
+};
+
+/*
+ * ROUTES
+ */
 app
-//.configure()
 .get('/hi', (req, res) => res.send("hello"))
 .get('/info', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify( { "name": APP_NAME, "version": APP_VERSION } ));
+    res.json( { "name": APP_NAME, "version": APP_VERSION } );
 })
 
-//TODO logs
-//TODO secure
-.post('/size/:name', (req, res) => {
+.all('/secure/*', requireAuthentication)
+
+.post('/secure/size/:name', (req, res) => {
     const name = req.params.name;
 
 })  
-.post('/weight/:name', (req, res) => {
+.post('/secure/weight/:name', (req, res) => {
     const name = req.params.name;
 
 })  
-.post('/shoes-size/:name', (req, res) => {
+.post('/secure/shoes-size/:name', (req, res) => {
     const name = req.params.name;
 
 })  
@@ -30,4 +45,4 @@ app
 .listen(9080);
 console.log('Familly-API started on server 9080');
 
- 
+
