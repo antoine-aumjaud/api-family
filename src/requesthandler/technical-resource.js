d95cbf8cf69a5877b1759ec0f56db7b2ffbf3652
Antoine Aumjaud <antoine_dev@aumjaud.fr>
2018-03-08 17:03:05 +0100

    :bug: change path conf JWT public key

--- src/requesthandler/technical-resource.js
+++ src/requesthandler/technical-resource.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require('express');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 const config = require('../conf/api-family.json');
 const commonConfig = require('../conf-common.json');
@@ -14,6 +16,23 @@ module.exports = express.Router()
     if(req.header("secure-key") === config.secureKey
     || req.query["secure-key"] === config.secureKey) {
         next();
+    }
+    else if(req.header("Authorization") != null) {
+        console.log(2);
+        const header = req.header("Authorization");
+        const token = header.substring(header.indexOf("Bearer") + 7);
+        const cert = fs.readFileSync('conf/jwt-public-cert.pem');  // get public key
+        jwt.verify(token, cert, function(err, decoded) {
+            if(err) {
+                console.log("SECURITY: invalid JWT, " + err);
+                res.status(401).send('Not authorized');
+            }
+            else {
+                req.header("name", decoded.name);
+                req.header("login", decoded.login);
+                next();
+            }
+        });
     }
     else {
         console.log("SECURITY: error on access to this resource")
