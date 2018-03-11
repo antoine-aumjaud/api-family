005447d4769db39aa1e81bd5154fb0435cfd98b2
Antoine Aumjaud <antoine_dev@aumjaud.fr>
2018-03-11 22:45:29 +0100

    :sparkles: use node library

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@
 
 const express    = require('express');
 const bodyParser = require('body-parser');
+const nodelib    = require('api-nodelib')
 
-const technicalResource = require('./requesthandler/technical-resource');
-const familyResource    = require('./requesthandler/family-resource');
+const technicalRouter = new nodelib.ExpressApp(
+    'conf-common.json', 
+    'conf/api-family.json').router();
+const familyResource = require('./requesthandler/family-resource');
 
 express()
 .use(bodyParser.json())
-.use('/', technicalResource)
+.use('/', technicalRouter)
 .use('/secure', familyResource)
 .listen(9080);
 console.log('Family-API started on server 9080');
